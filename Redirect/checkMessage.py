42509f70909b5b30f6c836503d6bb3b9fff11361
John <john@Evil-Sasha.(none)>
2013-11-28 11:50:48 -0800

    Changed msg checks

--- Redirect/checkMessage.py
+++ Redirect/checkMessage.py
@@ -2,11 +2,11 @@
 
 # All If statments are placeholders until I figure out what to put in them
 def check(msg):
-	if(msg == 1): #Valid Request
+	if(msg == 300): #Valid Request
 		return Messages.MULTIPLE_CHOICES
-	elif(msg == 2): #Moved
+	elif(msg == 301): #Moved
 		return Messages.MOVE_PERMANENT
-	elif(msg == 3): #Proxy
+	elif(msg == 305): #Proxy
 		return Messages.USE_PROXY
 	else:
 		return Messages.REJECTED
