e525f2392c1e7f0485e8bbc1181c6d4d4c6af74c
communistWatermelon <zyko101@gmail.com>
2013-11-28 12:42:45 -0800

    Added more code to Proxy

--- Proxy/ProcessMessage.py
+++ Proxy/ProcessMessage.py
@@ -1,23 +1,30 @@
-def processError(msg):
+def processError(msg, sock, addr):
 	print "Processing Error"
 
-def processAck(msg):
+def processAck(msg, sock, addr):
 	print "Processing ACK"
 
-def processRegister(msg):
+def processRegister(msg, sock, addr):
 	print "Processing REGISTER"
 
-def processInfo(msg):
+def processInfo(msg, sock, addr):
 	print "Processing INFO"
 
-def processInvite(msg):
+def processInvite(msg, sock, addr):
 	print "Processing INVITE"
+	sendOK(sock, addr)
 
-def processOptions(msg):
+def processOptions(msg, sock, addr):
 	print "Processing OPTION"
 
-def processCancel(msg):
+def processCancel(msg, sock, addr):
 	print "Processing CANCEL"
 
-def processBye(msg):
+def processBye(msg, sock, addr):
 	print "Processing BYE"
+
+	
+def sendOK(sock, addr):
+	print "sending OK"
+	sock.sendto("SIP/2.0 200 OK Via: SIP/2.0/UDP server10.biloxi.com ;branch=z9hG4bKnashds8;received=192.0.2.3 Via: SIP/2.0/UDP bigbox3.site3.atlanta.com ;branch=z9hG4bK77ef4c2312983.1;received=192.0.2.2 Via: SIP/2.0/UDP pc33.atlanta.com ;branch=z9hG4bK776asdhds ;received=192.0.2.1 To: Bob <sip:bob@biloxi.com>;tag=a6c85cf From: Alice <sip:alice@atlanta.com>;tag=1928301774 Call-ID: a84b4c76e66710@pc33.atlanta.com CSeq: 314159 INVITE Contact: <sip:bob@192.0.2.4> Content-Type: application/sdp Content-Length: 131", addr)
+	
--- Proxy/Proxy.py
+++ Proxy/Proxy.py
@@ -2,11 +2,14 @@ import socket
 import CheckMessage
 
 listenPort 		= 5060
-UDP_IP 			= "127.0.0.1"
+sendPort		= 3001
+UDP_IP 			= "192.168.56.1"
 listenSocket 	= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket 	= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientAddress 	= ((UDP_IP, sendPort))
 listenSocket.bind((UDP_IP, listenPort))
 
 while True:
 	message, addr = listenSocket.recvfrom(1024)
-	CheckMessage.options[CheckMessage.check(message)](message)
+	CheckMessage.options[CheckMessage.check(message)](message, clientSocket, clientAddress)
 
