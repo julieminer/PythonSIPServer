0f24e3fe2deca757eb3ae785461121985f8d135a
communistWatermelon <zyko101@gmail.com>
2013-11-28 13:56:11 -0800

    Added code so we can use sockets

--- SIP.py
+++ SIP.py
@@ -1,5 +1,18 @@
+import socket
 from Proxy import Proxy 
 from Utilities import Utility
+import Registrar
+
+regAddress = "localhost"
+redAddress = "localhost" 
+locAddress = "localhost"
+
+RegistrarSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+RegistrarSocket.bind((regAddress, 5061))
+RedirectSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+RedirectSocket.bind((redAddress, 5062))
+LocationSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+LocationSocket.bind((locAddress, 5063))
 
 U = Utility.Utility()
 Proxy.start(U)
