ebc91810f8191cce1ba5e3e922b4cdd411630343
communistWatermelon <zyko101@gmail.com>
2013-11-28 13:47:45 -0800

    Added more comments

--- Proxy/ProcessMessage.py
+++ Proxy/ProcessMessage.py
@@ -7,23 +7,27 @@ def processAck(msg, utils):
 
 def processRegister(msg, utils):
 	print "Processing REGISTER"
-	# send to Registrar
+	# forwardToRegistrar(msg)
 
 def processInfo(msg, utils):
 	print "Processing INFO"
 
 def processInvite(msg, utils):
 	print "Processing INVITE"
-	# send trying
-	# forward to client
+	# send trying to client
+	# LOOKUP username
+	# send to address
 
 def processOptions(msg, utils):
 	print "Processing OPTION"
+	# don't do this
 	
 def processCancel(msg, utils):
 	print "Processing CANCEL"
+	# this sucks, ignore it
 
 def processBye(msg, utils):
 	print "Processing BYE"
+	# send bye to whoever
 	
 	
