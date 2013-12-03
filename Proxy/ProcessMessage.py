7775765e6407e6885f3654d56e2df00033b6d7be
communistWatermelon <zyko101@gmail.com>
2013-12-03 13:04:00 -0800

    Fixed untested code. Placeholders are mostly done

--- Proxy/ProcessMessage.py
+++ Proxy/ProcessMessage.py
@@ -15,13 +15,11 @@ def processInfo(msg, utils, socks):
 
 def processInvite(msg, utils, socks):
 	print "Processing INVITE"
-	socks[0].send(msg)
+	#socks[0].send(msg)
 	sendTrying(msg)
-	if((user = lookupUser(msg)) != "NO USER"):
+	user = lookupUser(msg, socks)
+	if(user != "NO USER"):
 		sendInvite(user)
-	# send trying to client
-	# LOOKUP username
-	# send to address
 
 def processOptions(msg, utils, socks):
 	print "Processing OPTION"
@@ -33,28 +31,34 @@ def processCancel(msg, utils, socks):
 
 def processBye(msg, utils, socks):
 	print "Processing BYE"
+	
 	# send bye to whoever
 	
 def sendTrying(msg):
+	print "send trying"
 	#get the address from the msg
 	#send TRYING message to address
 
 def sendInvite(user):
+	print "Send Invite"
 	#send invite to users address
 
-def lookupUser(msg):
-	#send message to location server
+def lookupUser(msg, socks):
 	name = getName(msg)
-	socks[2].send("LOOKUP " + name)
-	address = socks[2].recv(1024)
-	#wait for response to come back
-	#if no user exists, return NO USER
+	#socks[2].send("LOOKUP " + name)
+	#address = socks[2].recv(1024)
+	address = "192.168.0.1"
 	if(address == ""):
 		return "NO USER"
 	return address
 
 def getName(msg):
-	nameStart 	= msg.find("To: ")
+	print "getting name"
+	nameStart 	= msg.find(";tag")
 	nameEnd 	= msg.find("From: ")
 	nameSize	= nameEnd - nameStart
-	return msg.read(nameSize, nameStart)
+	print "namestart = ", nameStart
+	print "nameEnd = ", nameEnd 
+	name  		= msg[nameEnd:nameStart]
+	print name
+	return name
