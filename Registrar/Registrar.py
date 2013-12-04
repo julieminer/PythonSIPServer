407cb78a325c39dec1cfa1d654d221ad9d746090
luketao <coolman0893@gmail.com>
2013-12-03 16:22:10 -0800

    Sending SIP address from Registrar to Location works

--- Registrar/Registrar.py
+++ Registrar/Registrar.py
@@ -6,61 +6,52 @@ ProxyListenPort = 5061
 LocationSendPort = 5062
 Proxy_IP = "127.0.0.1"
 Location_IP = "127.0.0.1"
-regMsg = ''
 
 class ProxyThread(threading.Thread):
-    def __init__(self, ip, port, proxySock):
+    def __init__(self, proxy_ip, proxy_port, proxySock):
         threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.proxySock = proxySock
+        self.location_ip = Location_IP
+        self.location_port = LocationSendPort
+        self.location_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.proxy_ip = proxy_ip
+        self.proxy_port = proxy_port
+        self.proxy_sock = proxySock
         print "New thread started for Proxy"
 
     def run(self):
-        print "Connection from Proxy: " + self.ip
+        print "Connection from Proxy: " + self.proxy_ip
                 
-        while True:
-            recvMessage = self.proxySock.recv(1024)
+        recvMessage = self.proxy_sock.recv(1024)
             
-            if(Messages.checkRegister(recvMessage) == True):
-                regMsg = Messages.parseMsg(recvMessage)
-                print regMsg
-            else:
-                continue
-                
-class LocationThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.ip = Location_IP
-        self.port = LocationSendPort
-        self.locateSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print "New thread started for location"
+        if(Messages.checkRegister(recvMessage) == True):
+            regMsg = Messages.parseMsg(recvMessage)
+            print regMsg
+            
+            self.location_sock.connect((self.location_ip, self.location_port))
+            print "Connect with location"            
+            self.location_sock.send(regMsg)
+            print "Sending location register id"
 
-    def run(self):
-        self.locateSock.connect((self.ip, self.port))
-        print "Connection to Location: " + self.ip
+            response = self.location_sock.recv(1024)
+            print "Recv from location: " + response
 
-        while True:
-            print "Sending..."
-            if (len(regMsg) != 0):
-                self.locateSock.send("Hi")
-                print "Sent hi"
-            print "Not sending"
+            self.location_sock.close()
+            print "Close Connection with location"
+            self.proxy_sock.send("OK")
+            print "Send OK to proxy"
 
 if __name__ == '__main__':
     #Create listen for proxy socket, wait for connection and process thread
 
+    while True:
+        #Create socket and connect to Proxy Server
+        proxySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxySock.bind((Proxy_IP,ProxyListenPort))
+        proxySock.listen(1)
+        (pSock, (Proxy_IP, ProxyListenPort)) = proxySock.accept()
+        proxyThread = ProxyThread(Proxy_IP, ProxyListenPort, pSock)    
+        proxyThread.start()
+        proxySock.close()
+        
     
-    proxySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    proxySock.bind((Proxy_IP,ProxyListenPort))
-    proxySock.listen(1)
 
-    (pSock, (Proxy_IP, ProxyListenPort)) = proxySock.accept()
-
-    proxyThread = ProxyThread(Proxy_IP, ProxyListenPort, pSock)
-    
-    proxyThread.start()
-    locationThread.start()
-
-    proxyThread.join()
-    locationThread.join()
